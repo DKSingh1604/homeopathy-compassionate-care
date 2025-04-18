68e824aab20a96c25ef512f9d76343766fc30a59
Dev Karan Singh <dev1604karan@gmail.com>
2025-04-18 15:30:13 +0530

    updated

--- src/components/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp.tsx
@@ -1,5 +1,6 @@
 
 import * as React from "react";
+
 import { MessageCircle } from "lucide-react";
 
 const FloatingWhatsApp = () => {
@@ -11,7 +12,13 @@ const FloatingWhatsApp = () => {
       aria-label="WhatsApp quick chat"
       className="fixed bottom-6 right-6 z-50 bg-green-600 hover:bg-green-700 text-white p-4 rounded-full shadow-lg transition-colors flex items-center justify-center"
     >
-      <MessageCircle className="w-6 h-6" />
+
+      {/* <MessageCircle className="w-6 h-6" /> */}
+      <img
+        src="/public/whatsapp.png"
+        alt="WhatsApp Icon"
+        className="w-6 h-6"
+      />
     </a>
   );
 };
